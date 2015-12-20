b582360eb1dadb3f89657b110efb42fc3dcfaa8b
Ivan Liljeqvist <ivanli@kth.se>
2015-12-20 12:30:44 +0100

    modular

---
+++ modules/globals.lua
@@ -0,0 +1,9 @@
+-- Put functions in this file to use them in several other scripts.
+-- To get access to the functions, you need to put:
+-- require "my_directory.my_file"
+-- in any script using the functions.
+
+TILE_SIZE=128
+TILE_NOT_REACHABLE_CODE=0
+
+JEEP_NAME="ARMED JEEP"
---
+++ modules/state.lua
@@ -0,0 +1,12 @@
+-- Put functions in this file to use them in several other scripts.
+-- To get access to the functions, you need to put:
+-- require "my_directory.my_file"
+-- in any script using the functions.
+
+selectableUnits={}
+mouseDown=false
+savedMouseDownCoordinates=false
+mouseDownStartCoordinates={0,0}
+massSelectionMode=false
+
+
