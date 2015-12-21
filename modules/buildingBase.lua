09a46f0328c108ec3ebaf44baa37ee4bccd23530
Ivan Liljeqvist <ivanli@kth.se>
2015-12-21 08:29:29 +0100

    building base

---
+++ modules/buildingBase.lua
@@ -0,0 +1,17 @@
+
+
+function initBuilding(self)
+	self.prototypeMode=true
+end
+
+function buildingInput(self,action)
+	--if prototype mode - follow the cursor
+	if self.prototypeMode then
+		--follow cursor
+	end
+end
+
+function buildingUpdate(self,dt)
+
+end
+
