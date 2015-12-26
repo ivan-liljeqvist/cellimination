2baa21dad12467179670350d67355166c78f1f81
Ivan Liljeqvist <ivanli@kth.se>
2015-12-26 08:05:24 +0100

    fog of war bug fixed

--- audio/mixer.go
+++ audio/mixer.go
@@ -14,10 +14,30 @@ components {
   }
 }
 embedded_components {
-  id: "sound"
+  id: "battle"
   type: "sound"
   data: "sound: \"/audio/battle.ogg\"\n"
   "looping: 1\n"
+  "group: \"master\"\n"
+  "gain: 1.0\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "calm"
+  type: "sound"
+  data: "sound: \"/audio/workingMusic.ogg\"\n"
+  "looping: 0\n"
   "group: \"master\"\n"
   "gain: 1.0\n"
   ""
