69c15aae244e7bb0995b83472ecc6002353e8501
Ivan Liljeqvist <ivanli@kth.se>
2015-12-28 03:14:47 +0100

    refactor

--- buildings/buildingManager.go
+++ buildings/buildingManager.go
@@ -1,5 +1,5 @@
 components {
-  id: "factory"
+  id: "baseFactory"
   component: "/buildings/base/base.factory"
   position {
     x: 0.0
@@ -16,6 +16,57 @@ components {
 components {
   id: "script"
   component: "/buildings/buildingManager.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "carbExtractorFactory"
+  type: "factory"
+  data: "prototype: \"/buildings/carbExtractor/carbExtractor.go\"\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "fatExtractor"
+  type: "factory"
+  data: "prototype: \"/buildings/fatExtractor/fatExtractor.go\"\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "proteinExtractorFactory"
+  type: "factory"
+  data: "prototype: \"/buildings/proteinExtractor/proteinExtractor.go\"\n"
+  ""
   position {
     x: 0.0
     y: 0.0
---
+++ buildings/carbExtractor/carbExtractor.go
@@ -0,0 +1,15 @@
+components {
+  id: "script"
+  component: "/buildings/carbExtractor/carbExtractor.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
